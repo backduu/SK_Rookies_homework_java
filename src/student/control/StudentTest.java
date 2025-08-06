7d2e513d1e33c4b9219e337032cc20783220f2fb
backduu <power4290@gmail.com>
2025-08-06 15:59:14 +0900

    try~catch문으로 변경하기

--- src/student/control/StudentTest.java
+++ src/student/control/StudentTest.java
@@ -4,11 +4,16 @@ import student.entity.Student;
 
 public class StudentTest {
 
-	public static void main(String[] args) throws InvalidSettingException {
+	public static void main(String[] args){
 		// TODO Auto-generated method stub
 		Student backduu = new Student("100", "백두현", "컴퓨터공학과", 4);
 		
-		backduu.setGrade(5);
+		try {
+			backduu.setGrade(5);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
